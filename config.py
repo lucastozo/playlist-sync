e9505cd045facf9b897a995d878f3ded511ecfad
Lucas Tozo <lucastozo7@gmail.com>
2025-06-15 03:37:21 -0400

    Revert "Change track number property"
    This reverts commit 49c7140b6d860001c1a400dc2bfffed51aa79a28.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ COOKIES_FILE_PATH = "" # Leave it empty if you do not need cookies file. Example
 
 EMBED_THUMBNAIL = False # Download the video thumbnail
 PLAYLIST_TRACKING = False # This will keep a file inside the OUTPUT_FOLDER_PATH containing all URLs downloaded from the playlist. This is useful to not waste time trying to download videos that already exists
-CHANGE_TRACK_NUMBER = False # Enabling this will change the Track number property to match the order of the playlist, you can use this to sort in your music player
+CHANGE_MODIFIED_DATE = False # Enabling this will make the Date modified property the same as Date created
 SILENT_MODE = False # Enabling this will hidden yt-dlp download info on terminal
 
 AUDIO_PARAMS = "-f bestaudio -x --audio-format mp3 --audio-quality 320k"
@@ -12,8 +12,8 @@ ADDITIONAL_PARAMS = "" # Additional yt-dlp options you may want to use
 
 # APP INFO AND SETTINGS, DO NOT CHANGE VALUES BELOW
 APP_NAME = "playlist-sync"
-VERSION = "1.0.1"
+VERSION = "1.0.0"
 PLAYLIST_FILE_NAME = ".playlist"
-TRACKING_FILE_NAME = ".tracking"
+TRACKING_FILE_NAME = "tracking"
 
 YOUTUBE_PLAYLIST_URL_PREFIX = ["https://youtube.com/playlist?list=", "https://www.youtube.com/playlist?list="]
